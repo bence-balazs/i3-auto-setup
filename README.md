2549ec0aadf8e37bb2213ebda33718b52bf350e7
bence.balazs <bence.balazs@qanplatform.com>
2024-01-22 12:12:27 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # i3-auto-setup
+
+Login as root user
+
+```sh
+apt install git -y
+```
+
+```
+git clone https://github.com/bence-balazs/i3-auto-setup.git
+```
+
+then run the ```i3setup.sh``` -script as root user.
---
+++ i3setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e 
+
+if [ $UID -ne 0 ]; then
+    echo "Must be root user to run this script!"
+    exit 1
+else
+    echo "This script will setup a bare i3 VM with some neccessary packages."
+fi
+
+apt update -y
+apt install sudo -y
+sudo apt install i3 xorg lightdm vim xfce4-terminal thunar spice-vdagent spice-webdavd -y
